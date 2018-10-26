686fa4959e6a7b9720e4ec81843cf6e7862d56ac
ROOPKANTH-KURRA <kurraroopkanth@gmail.com>
2018-10-26 14:11:06 +0530

    Delete sonar.groovy

--- my-app/sonar.groovy
+++
@@ -1,8 +0,0 @@
-try {
-stage("Building SONAR ...") {
-sh './gradlew clean sonarqube'
-}
-} catch (e) {emailext attachLog: true, body: 'See attached log', subject: 'BUSINESS Build Failure', to: 'abc@gmail.com'
-step([$class: 'WsCleanup'])
-return
-}
